a4929c1f4a9e4acb67f23252767a9b17a042a556
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-30 20:20:41 +0530

    Support SUPABASE_URL environment variable name

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get('SUPABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600
     )
 }
